ba9d6f74b1b44cc2e0890b42214c2eb7822da01b
rodion <rodiongork@github.com>
2019-12-30 16:51:31 +0300

    Klotski working w/o multistep

--- klotski/klotski.go
+++ klotski/klotski.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 import "sort"
 import "strconv"
+import "strings"
 
 var nei = [][]int{{1, 0}, {0, -1}, {-1, 0}, {0, 1}}
 
 var board [][]byte
-var boardsSeen = make(map[string]bool)
+var boardsSeen = make(map[string]string)
 var boardsToCheck = make([]string, 0)
 var congr map[byte]byte
 var width int
@@ -16,9 +17,14 @@ var height int
 func main() {
     readBoard()
     congruency()
-    addPosition()
+    addPosition("x")
     for len(boardsToCheck) > 0 {
-        setPosition(boardsToCheck[0])
+        b := boardsToCheck[0]
+        setPosition(b)
+        if checkGoal() {
+            traceBack()
+            break
+        }
         boardsToCheck = boardsToCheck[1:]
         findMoves()
     }
@@ -43,10 +49,7 @@ func congruency() {
     for _, row := range board {
         for _, v := range row {
             if v == '.' || congr[v] != 0 { continue }
-            sq := findSquares(v)
-            sort.Slice(sq, func(i, j int) (bool) {
-                return sq[i][0] * 256 + sq[i][1] < sq[j][0] * 256 + sq[j][1]
-            })
+            sq := findSquaresSorted(v)
             x0 := sq[0][0]
             y0 := sq[0][1]
             s := ""
@@ -61,26 +64,30 @@ func congruency() {
     }
 }
 
-func addPosition() (bool) {
-    s := ""
+func describePosition() (string, string) {
+    exact := ""
     for _, row := range board {
-        for _, v := range row {
-            if v != '.' {
-                s += string(congr[v])
-            } else {
-                s += "."
-            }
+        exact += string(row)
+    }
+    b := []byte(exact)
+    for i, v := range b {
+        if v != '.' {
+            b[i] = congr[v]
+        } else {
+            b[i] = '.'
         }
     }
-    if boardsSeen[s] {
+    plain := string(b)
+    return exact, plain
+}
+
+func addPosition(parent string) (bool) {
+    exact, plain := describePosition()
+    if boardsSeen[plain] != "" {
         return false
     }
-    boardsSeen[s] = true
-    s = ""
-    for _, row := range board {
-        s += string(row)
-    }
-    boardsToCheck = append(boardsToCheck, s)
+    boardsSeen[plain] = parent + " " + exact
+    boardsToCheck = append(boardsToCheck, exact)
     return true
 }
 
@@ -101,6 +108,7 @@ func printPosition() {
 func findMoves() {
     fmt.Println("Trying position:")
     printPosition()
+    _, plain := describePosition()
     empties := findSquares('.')
     for _, e := range empties {
         for dir, n := range nei {
@@ -112,7 +120,7 @@ func findMoves() {
                 if id > '.' && movePossible(id, dirMove) {
                     fmt.Println("MOVE:", string(id), dirMove)
                     makeMove(id, dirMove)
-                    if addPosition() {
+                    if addPosition(plain) {
                         printPosition()
                     } else {
                         fmt.Println("skip...")
@@ -138,6 +146,14 @@ func findSquares(id byte) ([][]int) {
         }
     }
     return res
+}
+
+func findSquaresSorted(id byte) ([][]int) {
+    sq := findSquares(id)
+    sort.Slice(sq, func(i, j int) (bool) {
+        return sq[i][0] * 256 + sq[i][1] < sq[j][0] * 256 + sq[j][1]
+    })
+    return sq
 }
 
 func movePossible(id byte, dir int) (bool) {
@@ -167,5 +183,25 @@ func makeMove(id byte, dir int) {
     dy := nei[dir][1]
     for _, s := range sq {
         board[s[1] + dy][s[0] + dx] = id
+    }
+}
+
+func checkGoal() (bool) {
+    goalId := byte('B')
+    goalX, goalY := 1, 3
+    sq := findSquaresSorted(goalId)
+    return sq[0][0] == goalX && sq[0][1] == goalY
+}
+
+func traceBack() {
+    fmt.Println("TRACEBACK!!!")
+    for i := 0; true; i +=1 {
+        printPosition()
+        fmt.Println("-", i)
+        _, plain := describePosition()
+        s := strings.Split(boardsSeen[plain], " ")
+        if s[0] == "x" { break }
+        s2 := strings.Split(boardsSeen[s[0]], " ")
+        setPosition(s2[1])
     }
 }
